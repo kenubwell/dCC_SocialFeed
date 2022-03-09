4269d444e6a98f0bb49566138a8b44c95279aa77
unknown <kenblackuoo@gmail.com>
2022-03-09 11:13:38 -0500

    Updating css styling in the NavBar component

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import './NavBar.css';
 
 const NavBar = () => {
-    return (  
-        
-        <div className ='nav-background'>
-            <h1>Social<small className= 'text-muted'>Feed</small></h1>
+    return (     
+    <div className ="nav-background">
+        <div className = "logo">
+            <h1><a href="http://localhost:3000/" className = "logo-decoration">Social<small className= 'text-muted'>Feed</small></a></h1>
         </div>
+        <nav>
+            <li className = "navbar-list"><a href="https://github.com/kenubwell/dCC_SocialFeed" className = "navbar-ref" target="_blank">GitHub Repo</a></li>
+            <li className = "navbar-list"><a href="https://www.w3.org/html/wg/wiki/Feed" className = "navbar-ref" target="_blank">What is a Feed?</a></li>
+            <li className = "navbar-list"><a href="https://devcodecamp.com/" target="_blank"><img src ="/images/devCodeCamp-Logo-12W.jpeg" className = "navbar-image" alt = "devCodeCamp Logo"/></a></li>
+        </nav>
+    </div>
+
     );
 }
  
